5f84928859837f9d5df7ddcd418c8cdf58d97c2b
LauriHe <Lauri.Heikkinen5@metropolia.fi>
2023-10-03 17:23:01 +0300

    layout fix

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -3,8 +3,8 @@ import { Outlet } from 'react-router-dom';
 function Layout() {
   return (
     <div className="flex flex-col h-[100vh]">
-      <nav className="h-20"></nav>
-      <main className="flex-grow">
+      <nav className="h-20 flex-shrink-0"></nav>
+      <main className="flex-grow flex-shrink">
         <Outlet />
       </main>
     </div>
