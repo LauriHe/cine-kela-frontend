22b65bc00c965763b2a778b3fbb07e610095183a
Nomsku <albin.sihvola@metropolia.fi>
2023-10-03 19:47:26 +0300

    base

--- src/views/Schedule.jsx
+++ src/views/Schedule.jsx
@@ -1,7 +1,23 @@
 function Schedule() {
   return (
-    <div>
-      <h1 className=" font-bold">teppo</h1>
+    <div className=" flex h-screen bg-oc-desert-yellow justify-center items-center">
+      <div className="flex h-4/6 w-5/6 bg-oc-space-blue rounded-md">
+        <div className=" bg-oc-hover-pink grow shrink hover:grow-[10] h-full rounded-l-md">
+          Mon
+        </div>
+        <div className=" bg-oc-pastel-blue grow shrink  hover:grow-[10] h-full">
+          Mon
+        </div>
+        <div className=" bg-oc-hover-pink grow shrink  hover:grow-[10] h-full">
+          Mon
+        </div>
+        <div className=" bg-oc-pastel-blue grow shrink  hover:grow-[10] h-full">
+          Mon
+        </div>
+        <div className=" bg-oc-hover-pink grow shrink  hover:grow-[10] h-full rounded-r-md">
+          Mon
+        </div>
+      </div>
     </div>
   );
 }
