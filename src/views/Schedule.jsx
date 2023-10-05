f8cdd713e7850d26b83b0a781bee268e6c96a395
Nomsku <albin.sihvola@metropolia.fi>
2023-10-05 13:18:09 +0300

    sfa

--- src/views/Schedule.jsx
+++ src/views/Schedule.jsx
@@ -1,13 +1,13 @@
 function Schedule() {
   return (
     <div className=" flex h-screen bg-oc-desert-yellow justify-center items-center">
-      <div className="flex sm:flex-col lg:flex-row sm:w-screen sm:h-screen lg:h-4/6 lg:w-5/6 bg-oc-space-blue rounded-md">
-        <div className=" card flex flex-col flex-1 bg-oc-hover-pink grow shrink sm:hover:grow-[20] lg:hover:grow-[10] lg:h-full rounded-l-md items-center">
+      <div className="flex xs:flex-col sm:flex-col lg:flex-row xs:w-screen sm:w-screen xs:h-screen sm:h-screen lg:h-4/6 lg:w-5/6 bg-oc-space-blue rounded-md">
+        <div className=" card flex flex-col flex-1 bg-oc-hover-pink grow shrink xs:hover:grow-[10] sm:hover:grow-[20] lg:hover:grow-[10] lg:h-full rounded-l-md items-center">
           <h1 className=" text-3xl font-semibold text-oc-desert-yellow">
             Monday
           </h1>
           <div className=" flex flex-col justify-center h-full items-center">
-            <video src="./public/fuji.mp4" className=" h p-4"></video>
+            <video src="./public/fuji.mp4" className=" lg:h-5/6 p-4"></video>
             <p className=" text-lg font-medium text-white p-4">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
               necessitatibus tenetur nam, placeat magnam est, nulla quaerat at,
@@ -21,7 +21,7 @@ function Schedule() {
             Tuesday
           </h1>
           <div className=" flex flex-col justify-center h-full items-center">
-            <video src="./public/fuji.mp4" className=" p-4"></video>
+            <video src="./public/fuji.mp4" className=" lg:h-5/6 p-4"></video>
             <p className=" text-lg font-medium text-white p-4">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
               necessitatibus tenetur nam, placeat magnam est, nulla quaerat at,
@@ -31,11 +31,11 @@ function Schedule() {
           </div>
         </div>
         <div className=" card flex flex-col flex-1 bg-oc-hover-pink grow shrink  hover:grow-[10] h-full items-center">
-          <h1 className=" text-3xl font-semibold text-oc-desert-yellow">
+          <h1 className=" px-4 text-3xl font-semibold text-oc-desert-yellow">
             Wednesday
           </h1>
           <div className=" flex flex-col justify-center h-full items-center">
-            <video src="./public/fuji.mp4" className=" p-4"></video>
+            <video src="./public/fuji.mp4" className=" lg:h-5/6 p-4"></video>
             <p className=" text-lg font-medium text-white p-4">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
               necessitatibus tenetur nam, placeat magnam est, nulla quaerat at,
@@ -49,7 +49,7 @@ function Schedule() {
             Thursday
           </h1>
           <div className=" flex flex-col justify-center h-full items-center">
-            <video src="./public/fuji.mp4" className=" p-4"></video>
+            <video src="./public/fuji.mp4" className=" lg:h-5/6 p-4"></video>
             <p className=" text-lg font-medium text-white p-4">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
               necessitatibus tenetur nam, placeat magnam est, nulla quaerat at,
@@ -63,7 +63,7 @@ function Schedule() {
             Friday
           </h1>
           <div className=" flex flex-col justify-center h-full items-center">
-            <video src="./public/fuji.mp4" className=" p-4"></video>
+            <video src="./public/fuji.mp4" className=" lg:h-5/6 p-4"></video>
             <p className=" text-lg font-medium text-white p-4">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
               necessitatibus tenetur nam, placeat magnam est, nulla quaerat at,
