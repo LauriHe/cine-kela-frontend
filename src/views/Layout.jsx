ebdb032dc585aff0b6b0c5d6fe3967959cdd2148
LauriHe <Lauri.Heikkinen5@metropolia.fi>
2023-10-08 02:08:21 +0300

    fixed layout

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,16 +1,22 @@
-import { Outlet } from "react-router-dom";
-import NavBar from "../components/NavBar";
-import Footer from "../components/footer";
+import { Outlet, useLocation } from 'react-router-dom';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 
 function Layout() {
+  const location = useLocation();
   return (
     <div className="flex flex-col h-[100vh]">
       <nav className="h-20 flex-shrink-0">
         <NavBar></NavBar>
       </nav>
-      <main className="bg-oc-space-blue">
+      <main className={location.pathname === '/stream' ? 'bg-oc-space-blue' : 'bg-oc-space-blue h-fit'}>
         <Outlet />
       </main>
+      {location.pathname !== '/stream' && (
+        <footer className="flex-shrink-0">
+          <Footer></Footer>
+        </footer>
+      )}
     </div>
   );
 }
