176795f980769e794c82d1a6c10d8fedc875de44
Nomsku <albin.sihvola@metropolia.fi>
2023-10-09 01:22:42 +0300

    tickets

--- src/views/Schedule.jsx
+++ src/views/Schedule.jsx
@@ -1,147 +1,191 @@
+import { Fragment, useState } from "react";
+import TicketForm from "../components/TicketForm";
+
 function Schedule() {
+  const [showModal, setShowModal] = useState(false);
   return (
-    <div>
-      <div className=" flex flex-col h-screen bg-oc-space-blue items-center">
-        <div className=" flex flex-col h-1/6 justify-center xs:my-4 sm:my-4">
-          <h1 className=" text-oc-cold-white font-bold text-5xl flex text-center">
-            Cinéma Kéla
-          </h1>
-          <h1 className=" text-oc-cold-white font-bold text-3xl text-center">
-            15.10 - 19.10
-          </h1>
-        </div>
-        <div className="flex xs:flex-col sm:flex-col lg:flex-row xs:w-screen sm:w-screen xs:h-screen sm:h-screen lg:h-4/6 lg:w-5/6 bg-oc-space-blue rounded-md">
-          <div className=" card flex flex-col flex-1 grow shrink xs:hover:grow-[10] sm:hover:grow-[20] lg:hover:grow-[10] lg:h-full rounded-md items-center lg:px-2 md:px-2 xs:py-2 sm:py-2 bg-oc-space-blue">
-            <h1 className=" day text-3xl font-semibold bg-oc-pastel-blue text-oc-cold-white w-full rounded-t-md text-center py-2">
-              Monday
+    <Fragment>
+      <div>
+        <div className=" flex flex-col h-screen bg-oc-space-blue items-center">
+          <div className=" flex flex-col h-1/6 justify-center xs:my-4 sm:my-4">
+            <h1 className=" text-oc-cold-white font-bold text-5xl flex text-center">
+              Ciné Kéla
             </h1>
-            <div className=" flex flex-col justify-center h-full items-center w-full bg-oc-cold-white rounded-b-md hover:rounded-md">
-              <video src="./public/fuji.mp4" className=" "></video>
-              <div className=" w-full flex flex-col">
-                <h2 className=" text-oc-cold-white text-xl  font-semibold py-2 bg-oc-pastel-blue w-full text-center">
-                  Jennifer's Body
-                </h2>
-                <h2 className=" text-oc-cold-white font-semibold py-2 bg-oc-pastel-blue w-full text-center">
-                  Starting at 12:30 - 15:30
-                </h2>
-              </div>
-              <h3 className=" w-full bg-oc-hover-pink font-semibold pl-4">
-                Horror, Comedy
-              </h3>
-              <p className=" text-lg font-medium text-oc-space-blue p-4 bg-oc-desert-yellow h-full w-full rounded-b-md">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
-                necessitatibus tenetur nam, placeat magnam est, nulla quaerat
-                at, recusandae esse aspernatur unde itaque quas alias omnis sit
-                laudantium neque? Eum?
-              </p>
-            </div>
+            <h1 className=" text-oc-cold-white font-bold text-3xl text-center">
+              15.10 - 19.10
+            </h1>
           </div>
-          <div className=" card flex flex-col flex-1 grow shrink  hover:grow-[10] h-full items-center rounded lg:px-2 md:px-2 bg-oc-space-blue xs:py-2 sm:py-2">
-            <h1 className=" day text-3xl font-semibold bg-oc-pastel-blue text-oc-cold-white w-full rounded-t-md text-center py-2">
-              Tuesday
-            </h1>
-            <div className=" flex flex-col justify-center h-full items-center w-full bg-oc-cold-white rounded-b-md">
-              <video src="./public/fuji.mp4" className=" "></video>
-              <div className=" w-full flex flex-col">
-                <h2 className=" text-oc-cold-white text-xl font-semibold py-2 bg-oc-pastel-blue w-full text-center">
-                  Ginger Snaps
-                </h2>
-                <h2 className=" text-oc-cold-white font-semibold py-2 bg-oc-pastel-blue w-full text-center">
-                  Starting at 12:30 - 15:30
-                </h2>
+          <div className="flex xs:flex-col sm:flex-col lg:flex-row xs:w-screen sm:w-screen xs:h-screen sm:h-screen lg:h-4/6 lg:w-5/6 bg-oc-space-blue rounded-md">
+            <div className=" card flex flex-col flex-1 shrink hover:grow-[10] rounded-md items-center lg:px-2 md:px-2 xs:py-2 sm:py-2 bg-oc-space-blue ">
+              <h1 className=" day px-4 text-3xl font-semibold bg-oc-pastel-blue text-oc-cold-white w-full rounded-t-md text-center py-2">
+                Monday
+              </h1>
+              <div className=" flex flex-col justify-center h-full items-center w-full bg-oc-cold-white rounded-b-md hover:rounded-md">
+                <video src="/placeholder.mp4" className=" "></video>
+                <div className=" w-full flex flex-col">
+                  <h2 className=" text-oc-cold-white text-xl  font-semibold py-2 bg-oc-pastel-blue w-full text-center">
+                    Jennifer's Body
+                  </h2>
+                  <h2 className=" text-oc-cold-white font-semibold py-2 bg-oc-pastel-blue w-full text-center">
+                    Starting at 12:30 - 15:30
+                  </h2>
+                </div>
+                <h3 className=" w-full bg-oc-hover-pink font-semibold pl-4">
+                  Horror, Comedy
+                </h3>
+                <p className=" text-lg font-medium text-oc-space-blue p-4 bg-oc-cold-white h-full w-full ">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Labore necessitatibus tenetur nam, placeat magnam est, nulla
+                  quaerat at, recusandae esse aspernatur unde itaque quas alias
+                  omnis sit laudantium neque? Eum?
+                </p>
+                <div className=" buttonDiv bg-oc-cold-white w-full flex justify-center items-center rounded-b-md pb-4">
+                  <button
+                    className=" text-lg font-semibold text-oc-cold-white bg-oc-rosy-pink hover:bg-oc-hover-pink rounded-md p-2 flex justify-center"
+                    onClick={() => setShowModal(true)}
+                  >
+                    Buy Ticket
+                  </button>
+                </div>
               </div>
-              <h3 className=" w-full bg-oc-hover-pink font-semibold pl-4">
-                Drama, Fantasy, Horror, Thriller
-              </h3>
-              <p className=" text-lg font-medium text-oc-space-blue p-4 bg-oc-desert-yellow h-full w-full rounded-b-md">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
-                necessitatibus tenetur nam, placeat magnam est, nulla quaerat
-                at, recusandae esse aspernatur unde itaque quas alias omnis sit
-                laudantium neque? Eum?
-              </p>
             </div>
-          </div>
-          <div className=" card flex flex-col flex-1 grow shrink  hover:grow-[10] h-full items-center rounded-md lg:px-2 md:px-2 bg-oc-space-blue xs:py-2 sm:py-2">
-            <h1 className=" day px-4 text-3xl font-semibold bg-oc-pastel-blue text-oc-cold-white w-full rounded-t-md text-center py-2">
-              Wednesday
-            </h1>
-            <div className=" flex flex-col justify-center h-full items-center w-full bg-oc-cold-white rounded-b-md hover:rounded-md">
-              <video src="./public/fuji.mp4" className=" "></video>
-              <div className=" w-full flex flex-col">
-                <h2 className=" text-oc-cold-white text-xl  font-semibold py-2 bg-oc-pastel-blue w-full text-center">
-                  Phantom of the Paradise
-                </h2>
-                <h2 className=" text-oc-cold-white font-semibold py-2 bg-oc-pastel-blue w-full text-center">
-                  Starting at 12:30 - 15:30
-                </h2>
+            <div className=" card flex flex-col flex-1 grow shrink  hover:grow-[10] h-full items-center rounded lg:px-2 md:px-2 bg-oc-space-blue xs:py-2 sm:py-2">
+              <h1 className=" day px-4 text-3xl font-semibold bg-oc-pastel-blue text-oc-cold-white w-full rounded-t-md text-center py-2">
+                Tuesday
+              </h1>
+              <div className=" flex flex-col justify-center h-full items-center w-full bg-oc-cold-white rounded-b-md">
+                <video src="/placeholder.mp4" className=" "></video>
+                <div className=" w-full flex flex-col">
+                  <h2 className=" text-oc-cold-white text-xl font-semibold py-2 bg-oc-pastel-blue w-full text-center">
+                    Ginger Snaps
+                  </h2>
+                  <h2 className=" text-oc-cold-white font-semibold py-2 bg-oc-pastel-blue w-full text-center">
+                    Starting at 12:30 - 15:30
+                  </h2>
+                </div>
+                <h3 className=" w-full bg-oc-hover-pink font-semibold pl-4">
+                  Drama, Fantasy, Horror, Thriller
+                </h3>
+                <p className=" text-lg font-medium text-oc-space-blue p-4 bg-oc-cold-white h-full w-full ">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Labore necessitatibus tenetur nam, placeat magnam est, nulla
+                  quaerat at, recusandae esse aspernatur unde itaque quas alias
+                  omnis sit laudantium neque? Eum?
+                </p>
+                <div className=" buttonDiv bg-oc-cold-white w-full flex justify-center items-center rounded-b-md pb-4">
+                  <button
+                    className=" text-lg font-semibold text-oc-cold-white bg-oc-rosy-pink hover:bg-oc-hover-pink  rounded-md p-2 flex justify-center"
+                    onClick={() => setShowModal(true)}
+                  >
+                    Buy Ticket
+                  </button>
+                </div>
               </div>
-              <h3 className=" w-full bg-oc-hover-pink font-semibold pl-4">
-                Drama, Music, Horror, Comedy
-              </h3>
-              <p className=" text-lg font-medium text-oc-space-blue p-4 bg-oc-desert-yellow h-full w-full rounded-b-md">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
-                necessitatibus tenetur nam, placeat magnam est, nulla quaerat
-                at, recusandae esse aspernatur unde itaque quas alias omnis sit
-                laudantium neque? Eum?
-              </p>
             </div>
-          </div>
-          <div className=" card flex flex-col flex-1 grow shrink  hover:grow-[10] h-full items-center rounded-md lg:px-2 md:px-2 bg-oc-space-blue xs:py-2 sm:py-2">
-            <h1 className=" day text-3xl font-semibold bg-oc-pastel-blue text-oc-cold-white w-full rounded-t-md text-center py-2">
-              Thursday
-            </h1>
-            <div className=" flex flex-col justify-center h-full items-center w-full bg-oc-cold-white rounded-b-md hover:rounded-md">
-              <video src="./public/fuji.mp4" className=" "></video>
-              <div className=" w-full flex flex-col">
-                <h2 className=" text-oc-cold-white text-xl  font-semibold py-2 bg-oc-pastel-blue w-full text-center">
-                  Moonlight
-                </h2>
-                <h2 className=" text-oc-cold-white font-semibold py-2 bg-oc-pastel-blue w-full text-center">
-                  Starting at 12:30 - 15:30
-                </h2>
+            <div className=" card flex flex-col flex-1 grow shrink  hover:grow-[10] h-full items-center rounded-md lg:px-2 md:px-2 bg-oc-space-blue xs:py-2 sm:py-2">
+              <h1 className=" day px-4 text-3xl font-semibold bg-oc-pastel-blue text-oc-cold-white w-full rounded-t-md text-center py-2">
+                Wednesday
+              </h1>
+              <div className=" flex flex-col justify-center h-full items-center w-full bg-oc-cold-white rounded-b-md hover:rounded-md">
+                <video src="/placeholder.mp4" className=" "></video>
+                <div className=" w-full flex flex-col">
+                  <h2 className=" text-oc-cold-white text-xl  font-semibold py-2 bg-oc-pastel-blue w-full text-center">
+                    Phantom of the Paradise
+                  </h2>
+                  <h2 className=" text-oc-cold-white font-semibold py-2 bg-oc-pastel-blue w-full text-center">
+                    Starting at 12:30 - 15:30
+                  </h2>
+                </div>
+                <h3 className=" w-full bg-oc-hover-pink font-semibold pl-4">
+                  Drama, Music, Horror, Comedy
+                </h3>
+                <p className=" text-lg font-medium text-oc-space-blue p-4 bg-oc-cold-white h-full w-full ">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Labore necessitatibus tenetur nam, placeat magnam est, nulla
+                  quaerat at, recusandae esse aspernatur unde itaque quas alias
+                  omnis sit laudantium neque? Eum?
+                </p>
+                <div className=" buttonDiv bg-oc-cold-white w-full flex justify-center items-center rounded-b-md pb-4">
+                  <button
+                    className=" text-lg font-semibold text-oc-cold-white bg-oc-rosy-pink hover:bg-oc-hover-pink  rounded-md p-2 flex justify-center"
+                    onClick={() => setShowModal(true)}
+                  >
+                    Buy Ticket
+                  </button>
+                </div>
               </div>
-              <h3 className=" w-full bg-oc-hover-pink font-semibold pl-4">
-                Drama
-              </h3>
-              <p className=" text-lg font-medium text-oc-space-blue p-4 bg-oc-desert-yellow h-full w-full rounded-b-md">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
-                necessitatibus tenetur nam, placeat magnam est, nulla quaerat
-                at, recusandae esse aspernatur unde itaque quas alias omnis sit
-                laudantium neque? Eum?
-              </p>
             </div>
-          </div>
-          <div className=" card flex flex-col flex-1 grow shrink  hover:grow-[10] h-full items-center rounded-md lg:px-2 md:px-2 bg-oc-space-blue xs:py-2 sm:py-2">
-            <h1 className=" day text-3xl font-semibold text-oc-cold-white w-full rounded-md text-center mb-2 bg-oc-pastel-blue py-2">
-              Friday
-            </h1>
-            <div className=" flex flex-col justify-center h-full items-center w-full bg-oc-cold-white rounded-md hover:rounded-md">
-              <video src="/fuji.mp4" className=" "></video>
-              <div className=" w-full flex flex-col">
-                <h2 className=" text-oc-cold-white text-xl  font-semibold py-2 bg-oc-pastel-blue w-full text-center">
-                  The Shape of Water
-                </h2>
+            <div className=" card flex flex-col flex-1 grow shrink  hover:grow-[10] h-full items-center rounded-md lg:px-2 md:px-2 bg-oc-space-blue xs:py-2 sm:py-2">
+              <h1 className=" day text-3xl font-semibold bg-oc-pastel-blue text-oc-cold-white w-full rounded-t-md text-center py-2">
+                Thursday
+              </h1>
+              <div className=" flex flex-col justify-center h-full items-center w-full bg-oc-cold-white rounded-b-md hover:rounded-md">
+                <video src="/placeholder.mp4" className=" "></video>
+                <div className=" w-full flex flex-col">
+                  <h2 className=" text-oc-cold-white text-xl  font-semibold py-2 bg-oc-pastel-blue w-full text-center">
+                    Moonlight
+                  </h2>
+                  <h2 className=" text-oc-cold-white font-semibold py-2 bg-oc-pastel-blue w-full text-center">
+                    Starting at 12:30 - 15:30
+                  </h2>
+                </div>
+                <h3 className=" w-full bg-oc-hover-pink font-semibold pl-4">
+                  Drama
+                </h3>
+                <p className=" text-lg font-medium text-oc-space-blue p-4 bg-oc-cold-white h-full w-full">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Labore necessitatibus tenetur nam, placeat magnam est, nulla
+                  quaerat at, recusandae esse aspernatur unde itaque quas alias
+                  omnis sit laudantium neque? Eum?
+                </p>
+                <div className=" buttonDiv bg-oc-cold-white w-full flex justify-center items-center rounded-b-md pb-4">
+                  <button
+                    className=" text-lg font-semibold text-oc-cold-white bg-oc-rosy-pink hover:bg-oc-hover-pink  rounded-md p-2 flex justify-center"
+                    onClick={() => setShowModal(true)}
+                  >
+                    Buy Ticket
+                  </button>
+                </div>
               </div>
-              <h3 className=" w-full bg-oc-hover-pink font-semibold pl-4">
-                Drama, Fantasy, Romance
-              </h3>
-              <h2 className=" text-oc-space-blue font-semibold py-2 bg-oc-test w-full text-center">
-                Starts at 12:30
-              </h2>
-              <p className=" text-lg font-medium text-oc-space-blue p-4 bg-oc-desert-yellow h-full w-full rounded-b-md">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
-                necessitatibus tenetur nam, placeat magnam est, nulla quaerat
-                at, recusandae esse aspernatur unde itaque quas alias omnis sit
-                laudantium neque? Eum?
-              </p>
+            </div>
+            <div className=" card flex flex-col flex-1 grow shrink  hover:grow-[10] h-full items-center rounded-md lg:px-2 md:px-2 bg-oc-space-blue xs:py-2 sm:py-2">
+              <h1 className=" day text-3xl font-semibold bg-oc-pastel-blue text-oc-cold-white w-full rounded-t-md text-center py-2">
+                Friday
+              </h1>
+              <div className=" flex flex-col justify-center h-full items-center w-full bg-oc-cold-white rounded-b-md hover:rounded-md">
+                <video src="/placeholder.mp4" className=" "></video>
+                <div className=" w-full flex flex-col">
+                  <h2 className=" text-oc-cold-white text-xl  font-semibold py-2 bg-oc-pastel-blue w-full text-center">
+                    Moonlight
+                  </h2>
+                  <h2 className=" text-oc-cold-white font-semibold py-2 bg-oc-pastel-blue w-full text-center">
+                    Starting at 12:30 - 15:30
+                  </h2>
+                </div>
+                <h3 className=" w-full bg-oc-hover-pink font-semibold pl-4">
+                  Drama
+                </h3>
+                <p className=" text-lg font-medium text-oc-space-blue p-4 bg-oc-cold-white h-full w-full">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Labore necessitatibus tenetur nam, placeat magnam est, nulla
+                  quaerat at, recusandae esse aspernatur unde itaque quas alias
+                  omnis sit laudantium neque? Eum?
+                </p>
+                <div className=" buttonDiv bg-oc-cold-white w-full flex justify-center items-center rounded-b-md pb-4">
+                  <button
+                    className=" text-lg font-semibold text-oc-cold-white bg-oc-rosy-pink hover:bg-oc-hover-pink  rounded-md p-2 flex justify-center"
+                    onClick={() => setShowModal(true)}
+                  >
+                    Buy Ticket
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <footer className=" text-center font-semibold bg-oc-hover-pink py-8 w-full">
-        Cinema Kela
-      </footer>
-    </div>
+      <TicketForm isVisible={showModal} onClose={() => setShowModal(false)} />
+    </Fragment>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'oc-space-blue': '#191e39',
-        'oc-pastel-blue': '#37436d',
-        'oc-desert-yellow': '#f4d4b8',
-        'oc-cold-white': '#edf3fd',
-        'oc-rosy-pink': '#d3979b',
-        'oc-hover-pink': '#B75259',
-        'oc-test': '#ECB483',
+        "oc-space-blue": "#191e39",
+        "oc-pastel-blue": "#37436d",
+        "oc-desert-yellow": "#f4d4b8",
+        "oc-cold-white": "#edf3fd",
+        "oc-rosy-pink": "#d3979b",
+        "oc-hover-pink": "#B75259",
+        "oc-test": "#ECB483",
       },
     },
     screens: {
-      xs: '400px',
+      xs: "400px",
 
-      s: '500px',
+      s: "500px",
 
-      sm: '640px',
+      sm: "640px",
 
-      md: '768px',
+      md: "768px",
 
-      lg: '1024px',
+      lg: "1024px",
 
-      xl: '1280px',
+      xl: "1280px",
 
-      '2xl': '1536px',
+      "2xl": "1536px",
     },
     backgroundSize: {
-      auto: 'auto',
-      cover: 'cover',
-      contain: 'contain',
-      icon: '1.7rem',
+      auto: "auto",
+      cover: "cover",
+      contain: "contain",
+      icon: "1.7rem",
     },
   },
   plugins: [],
